df27876220a71b412cf07ef13963b167351a3413
LuisEduardoBerrios <luiseduardoberrios@gmail.com>
2025-10-23 21:47:28 -0300

    Delete Aula 3 - Dados primitivos e funções.py

--- Aula 3 - Dados primitivos e funções.py
+++
@@ -1,9 +0,0 @@
-n1 = float(input("Digite o primeiro número: "))
-n2 = float(input("Digite o segundo número: "))
-
-print()
-
-print(f"O resultado da soma é: {n1 + n2}")
-print(f"O resultado da subtração é: {n1 - n2}")
-print(f"O resultado da multiplicação é: {n1 * n2}")
-print(f"O resultado da divisão é: {n1 / n2}")
