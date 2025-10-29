f9976456b1cafdbfb8ab13973ee9ceafb49af3ae
LuisEduardoBerrios <luiseduardoberrios@gmail.com>
2025-10-28 21:08:19 -0300

    Exercícios de fixação

---
+++ Atividade 5 - Funções de input e output/Atividade 5 - Funções de input e output.py
@@ -0,0 +1,53 @@
+# 1 Imprima uma mensagem de boas-vindas na tela.
+
+print("Hello world")
+
+# 2 Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo.
+
+x = True
+print(type(x))
+
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha.
+
+n1 = 5.5
+n2 = 6.1
+
+print(n1 * n2)
+
+# 4 Imprima o resultado da divisão (/)de dois números inteiros de sua escolha.
+
+n3 = 10
+n4 = 5
+
+print(n3 * n4)
+
+# 5 Imprima o resultado da subtração de dois números inteiros de sua escolha.
+
+print(n3 - n4)
+
+# 6 Imprima o resultado da divisão (//)inteira de dois números inteiros de sua escolha.
+
+print(n3 // n4)
+
+# 7 Imprima o resultado da multiplicação de 4 números decimais de sua escolha.
+
+n5 = 3.2
+n6 = 4.6
+
+print(n1*n2*n5*n6)
+
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, imprima o dobro desse número.
+
+var = 10
+print(var)
+
+# 9 Crie um sistema de cadastro com as estruturas que vc já conhece(Use apenas input, print e variavel) - nome, email, idade, cidade, gradução, estado civil.
+
+nome = input("Digite o seu nome: ")
+email = input("Digite o seu e=mail: ")
+idade = int(input("Qual é a sua idade: "))
+cidade = input("Qual é a sua cidade: ")
+graduação = input("Qual é o seu curso de graduação: ")
+estado_civil = input("Estado civil: ")
+
+print(nome, email, idade, cidade, graduação, estado_civil)
