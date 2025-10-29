30aed454887e0266e25e962bf3c0bed82fdea2bc
LuisEduardoBerrios <luiseduardoberrios@gmail.com>
2025-10-28 21:55:19 -0300

    Atividade 5 - Funções de input e output fixação

---
+++ Atividade 5 - Funções de input e output/Atividade 5.1 Exercicios de Fixação.py
@@ -0,0 +1,26 @@
+# Multiplique 4 por 6 e divida o resultado por 2. (Concatene com a palavra resultado)
+
+print(f"Resultado {4*6/2}")
+
+# Calcule a soma de 5 e 7.(Concatene com a palavra resultado)
+
+print(f"Resultado {5+7}")
+
+# Eleve 3 à potência de 4.(Concatene com a palavra resultado)
+
+print(f"Resultado {3**4}")
+
+# Calcule a média de 8, 12 e 15.(Concatene com a palavra resultado)
+
+media = (8+12+15)/3
+print("Resultado", media)
+
+# Subtraia 10 de 2 e multiplique o resultado por 5.(Concatene com a palavra resultado)
+
+operação = (10-2)*5
+print("Resultado", operação)
+
+# Divida 27 por 3 e some 5.(Concatene com a palavra resultado)
+
+div = (27*3)+5
+print("Resultado ", div)
