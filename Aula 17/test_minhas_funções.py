442dae0338359ef2ae86817eb7064db35009d5e1
LEB93 <luiseduardoberrios@gmail.com>
2025-11-14 21:27:20 -0300

    Add files via upload

---
+++ Aula 17/test_minhas_funções.py
@@ -0,0 +1,8 @@
+# site para teste = https://www.4devs.com.br/gerador_de_pessoas
+
+import minhas_funções as mf
+
+def test_somar():
+       assert mf.somar(2, 3) == 5
+       assert mf.somar(5, 7) == 12
+       assert mf.somar(10, -3) == 7
